849ed8cbd9d42eabcecd6627df9589db7fbac8de
amber12548 <amber12548@yahoo.com>
2021-11-14 16:45:16 -0600

    worked on some bugs still trying to get the calculations to match up and display properly on the dom

--- server/public/client.js
+++ server/public/client.js
@@ -1,46 +1,125 @@
 $(document).ready(onReady);
 
-function onReady (){
-  $('#equals').on('click', calcButtonPushed);
-}
+let buttonClicked = "";
+let holdingArray = [];
+
+// Create new functions for calculations buttons.
+// create seperate button to solve the calculation.
+    function onReady(){
+      console.log('Lets GO!!'); 
+      $('#add-btn').on('click', newButton)
+      $('#minus-btn').on('click', newButton)
+      $('#multiply-btn').on('click', newButton)
+      $('#divide-btn').on('click', newButton)
+      $('#clear-btn').on('click', newButton)
+      $('#equal-btn').on('click', solve)
+      calculationHistory();
+    }
 
 
-function calcButtonPushed(){
-  let equation = {
-    numOne: $('#firstNumberInput').val(),
-    operator: operator,
-    numTwo: $('#secondNumberInput').val(),
-  };
-  $('#firstNumberInput').val('');
-  $('#secondNumberInput').val('');
-  $.ajax ({
-    method: 'POST',
-    url: '/calculate',
-    data: equation
-  }).then ((response) => {
-    console.log(response);
-    $('#theCalcs').append(`
-    <li>${response}</li>
-    <li>${response}</li>
-    `);
-    handleRenderItems();
-  }).catch ((error) => {
-    console.log('error', error);
+// The newButton function brings in the clicked Id's buttons 
+// and reassigned it the variable buttonClicked
+
+    function newButton(){
+          if ($(this).attr('id') == "add-btn"){
+          buttonClicked = "add-btn";
+        }
+        else if ($(this).attr('id') == "minus-btn"){
+          buttonClicked = "minus-btn";
+        }
+      else if ($(this).attr('id') == "multiply-btn"){
+          buttonClicked = "multiply-btn";
+        }
+      else if ($(this).attr('id') == "divide-btn"){
+          buttonClicked = "divide-btn"; 
+        }
+    }
+
+      console.log('show results of buttonClicked', buttonClicked );
+
+// Create an object of the inputs from the inputs field of the calculation
+// send the information from the object to the server
+    function solve(){
+      let valuesObject = {
+        num1: $('#first-number').val(),
+        num2: $('#second-number').val(),
+        button: buttonClicked
+      }
+
+  // Push the information from the object into the empty holdingNumbers Array.
+  // log it to test it.
+      holdingArray.push(valuesObject);
+      console.log(holdingArray);
+
+  // Now, we need to send the information to the server by creating routes
+  // Using the Ajax method to send the response. 
+  // Check to ensure the response was send without error using .catch.
+
+      $.ajax({
+        method: 'POST',
+        url:'/values',
+        data: valuesObject,
+    }).then ( (response) => {
+      console.log('POST route /values', response);
+
+  // Ceate function to get results of calculation.
+    calculationResults();
+
+  // Create function to get the history of all calcualtions.
+    calculationHistory();
+
+    }).catch (error => {
+      console.log('POST route /values', error);
+    });
+  }
+
+// Create GET route request to push the calculation results to the DOM.
+    function calculationResults(){
+      $.ajax({
+      method: 'GET',
+      url: '/results'
+    }).then ((response) => {
+      console.log('GET route /results response', response);
+// Show the results to the DOM.
+      sumResults = $('#calculation').text(response[response.length - 1])
+      console.log(response);
+    }).catch (error => {
+      console.log('GETroute /values', error);
   });
 }
 
-function handleRenderItems() {
-  $.ajax ({
-    method: 'GET',
-    url: '/answer',
-  }).then ((response) => {
-    console.log('the response is:', response);
-    $('#theCalc').append(`
-    <li>${response}</li>
-    <li>${response}</li>
-    `);
-  }).catch ((error) => {
-    console.log('error', error);
+// Creat a function to post the history of all calculation to the DOM.
+  function calculationHistory() {
+      $.ajax({
+        method: 'GET',
+        url: '/history'
+        }).then((response) =>{
+          console.log('GET history response', response);
+// Create a new variable to append all history as a ul tag on the DOM.
+          let pastHistory = $('#history');
+          pastHistory.empty();
+// Create a loop to loop all GET responses.
+          for (let pushHistory of response ){
+            if (pushHistory.numBtn == "add-btn") {
+              buttonClicked = "+";
+          }
+          else if (pushHistory.numBtn == "minus-btn") {
+              buttonClicked = "-";
+          }
+          else if (pushHistory.numBtn == "multiply-btn") {
+              buttonClicked = "*";
+          }
+          else if (pushHistory.numBtn == "divide-btn") {
+              buttonClicked = "/";
+          }
+// push all past calculations History to the DOM using pastHistory by append it.
+    pastHistory.append(`
+      <li> ${pushHistory.num1} ${buttonClicked} ${pushHistory.num2} = ${pushHistory.sum}
+      </li>`)
+    }
   });
 }
+
+
+
 
--- server/server.js
+++ server/server.js
@@ -1,50 +1,73 @@
-let mathmaticalEquation =[];
+console.log('Server is working!');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
+
+let calculationArray=[];
+let HistoryArray = [];
+let sum = 0;
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
+
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
-app.get('/answer', (req, res) => {
-  console.log('hey express');
-  solveMathmaticalEquation();
-    res.send(mathmaticalEquation);
+
+app.post('/values', (req, res) =>{
+      console.log('req is', req.body);
+      calculationArray.push(req.body);
+      solvingMath()
+      console.log(calculationArray);
+      res.sendStatus(201);
+});
+app.get('/history', (req, res) => {
+    console.log('history path', req.body.path);
+    solvingMath();
+    res.send(HistoryArray);
+ });
+ //sending MathInputs to the GET function on the client side.
+      app.get('/results', (req, res) =>{
+      console.log('results path', req.route.path);
+      solvingMath();
+//it's being sent here.
+     res.send(calculationArray);
 });
 
+// create a function for if's conditional
 
-app.post('/calculate', (req, res) => {
-    console.log("show calculation:", req.body);
-    mathProblem.push(req.body);
-    console.log(mathmaticalEquation);
-    
-    res.send(201);
-  });
+  function solvingMath(){ 
+      for (let aNumber of calculationArray){
+        let fullSolve = {
+          sum: sum,
+          num1: aNumber.num1,
+          num2: aNumber.num2,
+          numBtn: aNumber.button
+        }
+      if (aNumber.button == "add-btn") {
+          sum = Number(aNumber.num1) + Number(aNumber.num2);
+          calculationArray.push(sum);
 
-//Send the mathmaticalEquations array to our front-end
-// app.get('/calculations', (req, res) => {
-//   console.log('in GET /calculations');
-  
-// })
-
- function solveMathmaticalEquation(){
-   for (let problem of mathmaticalEquation){
-     if(problem.operator  === '+'){
-       problem.answer = Number(items.numOne) + Number(items.numTwo);
-     }else if (problem.operator  === '-'){
-       problem.answer = Number(items.numOne) - Number(items.numTwo);
-     }else if (problem.operator === '*'){
-       problem.answer = Number(items.numOne) * Number(items.numTwo);
-     }else if (problem.operator === '/'){
-       problem.answer = Number(items.numOne) / Number(items.numTwo);
-   };
+    }
+      else if (aNumber.button == "minus-btn") {
+          sum = Number(aNumber.num1) - Number(aNumber.num2);
+          calculationArray.push(sum);
+    }
+      else if (aNumber.button == "multiply-btn") {
+          sum = Number(aNumber.num1) * Number(aNumber.num2);
+          calculationArray.push(sum);
+    }
+      else if (aNumber.button == "divide-btn") {
+          sum = Number(aNumber.num1) / Number(aNumber.num2);
+          calculationArray.push(sum);
+    }
+      HistoryArray.push(fullSolve);
   }
-   console.log(solveMathmaticalEquation);
-  console.log(MathmaticalEquation);
-  
-  };
+      console.log(HistoryArray);
+      console.log(calculationArray);
+}
 
-app.listen(5000, () => {
-  console.log('listening on port', PORT)
+    app.listen(PORT, (res) => {
+      console.log('listening on port', PORT)
 });
