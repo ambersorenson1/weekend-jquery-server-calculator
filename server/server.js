bb69e63ce5cf590a0e41640dc891cfeba15f1fc4
amber12548 <amber12548@yahoo.com>
2021-11-14 19:51:18 -0600

    updated the calculations

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-let arrayOfCalculations=[];
+let arrayOfCalculations= [];
 let previousInputedData = [];
 let sum = 0;
 
@@ -17,8 +17,8 @@ app.use(express.static('server/public'));
 
 app.post('/values', (req, res) =>{
       console.log('req is', req.body);
-      arrayOfCalculations.push(req.body);
-      mathEquationSolution()
+      //arrayOfCalculations.push(req.body);
+      mathEquationSolution(req.body)
       console.log(arrayOfCalculations);
       res.sendStatus(201);
 });
@@ -30,37 +30,37 @@ app.get('/history', (req, res) => {
       app.get('/results', (req, res) =>{
       console.log('results path', req.route.path);
 //it's being sent here.
-     res.send(arrayOfCalculations);
+     res.sendStatus(arrayOfCalculations);
 });
 
 // create a function for if's conditional
 
-  function mathEquationSolution(){ 
-      for (let number of arrayOfCalculations){
-        let fullSolve = {
-          sum: sum,
+  function mathEquationSolution(number){ 
+        let solution = {
           num1: number.num1,
           num2: number.num2,
           numBtn: number.button
         }
-      if (number.button == "add-btn") {
-          sum = Number(number.num1) + Number(number.num2);
-
+      if (number.button ==="add-btn") {
+          solution.sum = Number(number.num1) + Number(number.num2);
+            arrayOfCalculations.push(solution.sum)
     }
-      else if (number.button == "minus-btn") {
-          sum = Number(number.num1) - Number(number.num2);
+      else if (number.button === "minus-btn") {
+          solution.sum = Number(number.num1) - Number(number.num2);
+          arrayOfCalculations.push(solution.sum)
     }
-      else if (number.button == "multiply-btn") {
-          sum = Number(number.num1) * Number(number.num2);
+      else if (number.button === "multiply-btn") {
+          solution.sum = Number(number.num1) * Number(number.num2);
+          arrayOfCalculations.push(solution.sum)
     }
-      else if (number.button == "divide-btn") {
-          sum = Number(number.num1) / Number(number.num2);
+      else if (number.button === "divide-btn") {
+          solution.sum = Number(number.num1) / Number(number.num2);
+          arrayOfCalculations.push(solution.sum)
     }
-  previousInputedData.push(fullSolve);
+  previousInputedData.push(solution);
   }
       console.log(previousInputedData);
       console.log(arrayOfCalculations);
-}
 
     app.listen(PORT, (res) => {
       console.log('listening on port', PORT)
